c5a66af501459efc0e8fa224ebacd65317dfd3d0
zhangjunying1 <2595482128@qq.com>
2017-12-28 22:08:34 +0800

    'l1'

--- cc/js/component.js
+++ cc/js/component.js
@@ -15,7 +15,7 @@ Vue.component("custom-input",{
 
 Vue.component("custom-list",{
     props:["listData","state"],
-    template:`<ul class="select-list">
+    template:`<ul class="select-list" v-show="status">
         <li v-for="v in listData" @click="change(v.title)">{{v.title}}</li>
     </ul>`,
     data(){
